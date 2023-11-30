1e2d7679c094f89a996c93b0c9e23e90be9c91d3
thomas <inquister12345678@gmail.com>
2023-11-30 14:16:17 -0800

    set up express server

---
+++ server/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json()); 
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
