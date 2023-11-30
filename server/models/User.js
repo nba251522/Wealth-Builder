c6f4900669694d1faf04cca294a9da9e3ad37ce3
thomas <inquister12345678@gmail.com>
2023-11-30 14:53:20 -0800

    Added secure password hashing and comparison to User model using bcrypt and Mongoose pre-save hooks

--- server/models/User.js
+++ server/models/User.js
@@ -34,3 +34,15 @@ const userSchema = new mongoose.Schema({
     maxlength: 300
   },
 });
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) {
+      return next();
+    }
+    this.password = await bcrypt.hash(this.password, 8);
+    next();
+  });
+  userSchema.methods.comparePassword = async function(candidatePassword) {
+    return bcrypt.compare(candidatePassword, this.password);
+  };
+  
+  module.exports = mongoose.model('User', userSchema);
