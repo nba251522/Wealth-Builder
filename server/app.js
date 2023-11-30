a867cb70672b5809a061fcb2058835c12fec9389
thomas <inquister12345678@gmail.com>
2023-11-30 14:20:48 -0800

    updated my app,js to also set up the mongodb connection

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -9,3 +10,8 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
