317482687059c8dc70e8fc30ff34e6311711b42a
thomas <inquister12345678@gmail.com>
2023-11-30 14:46:31 -0800

    updated the user model to include basic information such as a username, email, password, etc

---
+++ server/models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    minlength: 3
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    match: [/.+\@.+\..+/, 'Please fill a valid email address']
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: Date,
+  profilePicture: String,
+  bio: {
+    type: String,
+    trim: true,
+    maxlength: 300
+  },
+});
