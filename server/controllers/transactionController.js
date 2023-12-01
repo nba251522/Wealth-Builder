fd21be59ca023d43a667901153868e3528df4f5a
thomas <inquister12345678@gmail.com>
2023-12-01 02:53:29 -0800

    updated the logic so that the user can update any existing transactions

--- server/controllers/transactionController.js
+++ server/controllers/transactionController.js
@@ -21,6 +21,7 @@ const transactionController = {
             res.status(400).json({ message: error.message });
         }
     },
+    // Get all transactions
     async getTransactions(req, res) {
         try {
             const transactions = await Transaction.find({ user: req.user.id });
@@ -29,13 +30,29 @@ const transactionController = {
             res.status(500).json({ message: error.message });
         }
     },
-    
+    // Fetch a single transaction
     async getTransaction(req, res) {
         try {
             const transaction = await Transaction.findById(req.params.id);
             res.json(transaction);
         } catch (error) {
             res.status(500).json({ message: error.message });
+        }
+    },
+
+    async updateTransaction(req, res) {
+        try {
+            const updatedTransaction = await Transaction.findByIdAndUpdate(
+                req.params.id,
+                req.body,
+                { new: true }
+            );
+            if (!updatedTransaction) {
+                return res.status(404).json({ message: 'Transaction not found' });
+            }
+            res.json(updatedTransaction);
+        } catch (error) {
+            res.status(400).json({ message: error.message });
         }
     },
 };
