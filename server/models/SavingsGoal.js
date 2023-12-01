f81c83553ae89c900f7f3ec1b510ae82c4138d29
thomas <inquister12345678@gmail.com>
2023-11-30 16:31:17 -0800

    completed the model for savings that has information such as goal amount, your current bank amount, and many other elements that a user would need when tracking their savings.

---
+++ server/models/SavingsGoal.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const savingsGoalSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  goalName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  goalAmount: {
+    type: Number,
+    required: true
+  },
+  currentAmount: {
+    type: Number,
+    default: 0
+  },
+  targetDate: {
+    type: Date
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: Date,
+});
+
+module.exports = mongoose.model('SavingsGoal', savingsGoalSchema);
