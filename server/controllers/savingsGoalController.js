eb4cc0c20700c56fa3a89b31eccf4787bc9d0bfa
thomas <inquister12345678@gmail.com>
2023-12-01 10:04:23 -0800

    added logic that will allow users to pull all savings goals that they have made

--- server/controllers/savingsGoalController.js
+++ server/controllers/savingsGoalController.js
@@ -18,7 +18,16 @@ const savingsGoalController = {
             res.status(400).json({ message: error.message });
         }
     },
+    async getSavingsGoals(req, res) {
+        try {
+            const savingsGoals = await SavingsGoal.find({ user: req.user.id });
+            res.json(savingsGoals);
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    },
 
 };
+
 
 module.exports = savingsGoalController;
