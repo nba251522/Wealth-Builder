8cbc70219b0f39cd78bc6a6d24e7dfd6829e9033
thomas <inquister12345678@gmail.com>
2023-11-30 23:39:04 -0800

    updated the userController file to contain login logix

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -7,7 +7,7 @@ const userController = {
     async register(req, res) {
         try {
             let user = await User.findOne({ email: req.body.email });
-            
+
             if (user) {
                 return res.status(400).json({ message: 'User already exists' });
             }
@@ -28,4 +28,25 @@ const userController = {
         }
     },
     
+    // Login an existing user
+     async login(req, res) {
+        try {
+            const user = await User.findOne({ email: req.body.email });
+            if (!user) {
+                return res.status(400).json({ message: 'Invalid credentials' });
+            }
+
+            const isMatch = await bcrypt.compare(req.body.password, user.password);
+            if (!isMatch) {
+                return res.status(400).json({ message: 'Invalid credentials' });
+            }
+
+            const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+            res.json({ token });
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    },
+
 };
