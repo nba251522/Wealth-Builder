da5a90cd6f3adc9daa841cab2ba27e83829e5137
thomas <inquister12345678@gmail.com>
2023-12-01 10:13:24 -0800

    added logic that will allow users to pull a single savings goal instead of having them pull all savings goals

--- server/controllers/savingsGoalController.js
+++ server/controllers/savingsGoalController.js
@@ -18,6 +18,7 @@ const savingsGoalController = {
             res.status(400).json({ message: error.message });
         }
     },
+    // Get all transactions
     async getSavingsGoals(req, res) {
         try {
             const savingsGoals = await SavingsGoal.find({ user: req.user.id });
@@ -26,7 +27,15 @@ const savingsGoalController = {
             res.status(500).json({ message: error.message });
         }
     },
-
+    // Fetch a single transaction
+    async getSavingsGoal(req, res) {
+        try {
+            const savingsGoal = await SavingsGoal.findById(req.params.id);
+            res.json(savingsGoal);
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    },
 };
 
 
