233e8d89d70f2f473c357826bfe0e7bb3d10679c
stevensillsii <stevensills2@gmail.com>
2023-12-08 11:04:06 -0800

    dbconfig.js is put together - I took the code from 23-Ins_Stripe cleanDB.js

---
+++ server/config/dbConfig.js
@@ -0,0 +1,17 @@
+// I  believe this is named the cleanDB.js in our class practice 
+const models = require('../models');
+const db = require('../config/connection');
+
+module.exports = async (modelName, collectionName) => {
+  try {
+    let modelExists = await models[modelName].db.db.listCollections({
+      name: collectionName
+    }).toArray()
+
+    if (modelExists.length) {
+      await db.dropCollection(collectionName);
+    }
+  } catch (err) {
+    throw err;
+  }
+}
