beb78e138e47972d947373aecbab20b56d6dc705
Ryan England <me@rengland.org>
2023-12-08 13:41:44 -0800

    Added basic logic for appearing/reappearing modules

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
+export default function App() {
+  // Create state for budget component
+  const [budget, setBudget] = useState(false);
+
+  function handleSelect(event, type) {
+    event.preventDefault();
+    switch(type) {
+      case "budget":
+        setBudget(!budget); // Toggle state of checkbox
+        break;
+      default:
+        break;
+    }
+  }
+
+  function Budget() {
+    if (budget) {
+      return <div>budget</div>; // return budget component when made avaible 
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page-content">
+      <div className="dropdown">
+          <button className="btn btn-primary dropdown-toggle" type="button" id="featureButton"
+          data-mdb-toggle="dropdown" aria-expanded="false">
+            Select a tracker!
+          </button>
+          <ul className="dropdown-menu" aria-labelledby="featureButton">
+              <li>
+                  <a className="dropdown-item">
+                      <div className="form-check">
+                          <input className="form-check-input" type="checkbox" id="budget"
+                          onChange={handleSelect("budget")}/>
+                          <label className="form-check-label" for="budget">Budget</label>
+                      </div>
+                  </a>
+              </li>
+          </ul>
+      </div>
+      { Budget() }
     </div>
   );
 }
 
-export default App;
