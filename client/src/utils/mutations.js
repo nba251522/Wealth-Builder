e6b7e47f3817676dceb10c1c3738ff60228f463f
stevensillsii <stevensills2@gmail.com>
2023-12-10 21:34:36 -0800

    made a mutations.js in the utils folder in the client, started making some mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,21 @@
+import { gql } from '@apollo/client';
+
+export const CREATE_USER = gql`
+    mutation createUser($username: String!, $email: String!, $password: String!) {
+        createUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+                email
+                budget
+                expenses {
+                    _id
+                    expenseText
+                    expenseAmount
+                    createdAt
+                }
+            }
+        }
+    }
+`;
