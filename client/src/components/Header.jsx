ac19ae2a725fa14490449d59f8be2310b46f1a12
Ryan England <me@rengland.org>
2023-12-12 11:41:57 -0800

    Added signup modal

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -34,18 +34,15 @@ const MyNavbar = () => {
       </Navbar.Collapse>
     </Navbar>
 
-    <Modal show={showLogin} onHide={handleClose}>
+    <Modal show={showLogin} onHide={handleLoginClose}>
         <Modal.Header closeButton>
           <Modal.Title>Login</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <Form>
             <Form.Group controlId="formUsername">
-              <Form.Label>Email address</Form.Label>
+              <Form.Label>Username</Form.Label>
               <Form.Control type="username" placeholder="Enter username" />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-                </Form.Text>
             </Form.Group>
             <Form.Group controlId="formPassword">
               <Form.Label>Password</Form.Label>
@@ -65,7 +62,43 @@ const MyNavbar = () => {
           </Button>
         </Modal.Footer>
       </Modal>
-      </>
+
+      <Modal show={showRegister} onHide={handleRegisterClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Sign Up</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="formEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter your email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+                </Form.Text>
+            </Form.Group>
+            <Form.Group controlId="formUsername">
+              <Form.Label>Username</Form.Label>
+              <Form.Control type="username" placeholder="Enter username" />
+            </Form.Group>
+            <Form.Group controlId="formPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Sign Up
+            </Button>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleRegisterClose}>
+            Close
+          </Button>
+          <Button variant="secondary" onClick={() => { handleRegisterClose(); handleLoginShow(); }}>
+            Have an account? Login
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 };
 
