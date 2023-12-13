d5d6215b83aa0e920e9c32d5a48c94d243c39ddd
thomas <inquister12345678@gmail.com>
2023-12-13 01:58:18 -0800

    rewrote the app.js to reference the class modules and import apollo correctly

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,40 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Outlet } from "react-router-dom";
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Budget from './components/Budget';
-import StockTracker from './components/StockTracker';
-import AboutUs from './components/AboutUs'
 
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: '/graphql'
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+})
 
 const App = () => {
   return (
-    <Router>
-      <div>
+    <>
+      <ApolloProvider client={client}>
         <Header />
-        <Routes>
-          <Route path='/' element={<Budget />} />
-          <Route path='/StockTracker' element={<StockTracker />} />
-          <Route path='/AboutUs' element={<AboutUs />} />
-        </Routes>
+        <main>
+          <Outlet />
+        </main>
         <Footer />
-      </div>
-    </Router>
+      </ApolloProvider>
+    </>
   );
 };
 
