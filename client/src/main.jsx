5e9c7106963aaed443075e51e9d6cd5bf96d6497
thomas <inquister12345678@gmail.com>
2023-12-13 02:04:08 -0800

    rewrote the main.jsx file to correctly import all of my dependencies

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import App from './App.jsx'
+import ErrorPage from './components/ErrorPage.jsx'
 
 
-const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import Budget from "./components/Budget.jsx";
+import StockTracker from "./components/StockTracker.jsx";
+import AboutUs from "./components/AboutUs.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Budget/>
+      },
+      {
+        path: '/tracker',
+        element: <StockTracker />
+      },
+      {
+        path: '/about',
+        element: <AboutUs />
+      }
+    ]
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+)
